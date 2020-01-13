a103d393fba2edb5ce4a29cf81bdeac669fe04d4
kimbjorkman <kimbjorkman@Kims-MacBook-Pro.local>
2020-01-13 15:09:51 +0100

    reworked lab 5

--- src/components/labs/lab5/index.js
+++ src/components/labs/lab5/index.js
@@ -1,38 +1,87 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect } from "react";
 import styles from "./lab.module.scss";
 import useEventListener from "../../../utils/hooks/useEventListener";
+import { lerp } from "../../../utils/helpers/animation";
 
 const FadeInTranslate = React.lazy(() =>
   import("../../animation-wrappers/fade-in-translate")
 );
 
+// start animation loop
+// set body height
+// listen for current scrolled amount
+// ease current scrolled value and transform content
 function Lab5() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const containerRef = useRef();
-  let currentPos = window.pageYOffset;
+  let contentEl = null;
+  let documentHeight = 0;
+  let scrollOffset = 0;
+  let prevScrollOffset = 0;
 
-  const onScroll = () => {
-    if (!containerRef.current) return null;
-    const newPos = window.pageYOffset;
-    const diff = newPos - currentPos;
-    const speed = diff * 0.05;
+  const initAnimationLoop = () => {
+    const render = () => {
+      prevScrollOffset = lerp(prevScrollOffset, scrollOffset, 0.1);
+      prevScrollOffset = Math.round(prevScrollOffset);
 
-    containerRef.current.style.transform = `skewY(${
-      newPos !== 0 ? speed : 0
-    }deg)`;
+      if (prevScrollOffset < 0.1) {
+        prevScrollOffset = 0;
+      }
 
-    currentPos = newPos;
+      const diff = Math.round(scrollOffset - prevScrollOffset);
+      const speed = diff * 0.01;
+
+      if (contentEl) {
+        contentEl.style.transform = `translate3d(0, -${prevScrollOffset}px, 0) skewY(${speed}deg)`;
+      }
+      requestAnimationFrame(render);
+    };
+    requestAnimationFrame(render);
   };
 
-  useEventListener("scroll", onScroll);
+  const handleScroll = () => {
+    // get scrolled amount
+    scrollOffset = window.pageYOffset || window.scrollTop || 0;
+  };
+
+  useEventListener("scroll", handleScroll);
+
+  const setBodyHeight = () => {
+    documentHeight = contentEl.getBoundingClientRect().height + 150;
+    document.body.style.height = `${documentHeight / 10}rem`;
+  };
+
+  const handleResize = () => {
+    setBodyHeight();
+  };
+
+  useEventListener("resize", handleResize);
+
+  // when ref is mounted check if the height has
+  // ... stabilized, set body height to the stable value
+  const refCallback = el => {
+    if (!el) return;
+    contentEl = el;
+    let prevValue = JSON.stringify(el.getBoundingClientRect());
+
+    const handle = setInterval(() => {
+      let nextValue = JSON.stringify(el.getBoundingClientRect());
+      if (nextValue === prevValue) {
+        // size stopped changing
+        clearInterval(handle);
+        setBodyHeight();
+      } else {
+        // size changed, check again in 100ms
+        prevValue = nextValue;
+      }
+    }, 100);
+  };
 
   useEffect(() => {
-    setIsLoaded(true);
-  }, []);
+    initAnimationLoop();
+  });
 
-  return isLoaded ? (
+  return (
     <div className={styles.root}>
-      <section ref={containerRef} className={styles.contentContainer}>
+      <section ref={refCallback} className={styles.contentContainer}>
         <FadeInTranslate fade_up>
           <h2 className={styles.headline}>Smooth Scrolling</h2>
           <p className={styles.body}>
@@ -97,10 +146,74 @@ function Lab5() {
             consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
             pariatur?
           </p>
+          <p className={styles.body}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+          <p className={styles.body}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+          <p className={styles.body}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+          <p className={styles.body}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
         </FadeInTranslate>
       </section>
     </div>
-  ) : null;
+  );
 }
 
 export default Lab5;
